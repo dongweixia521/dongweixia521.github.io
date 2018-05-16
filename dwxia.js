ca9f549d9519b810e3fd5f2fca19fcbaa81174e3
dongweixia521 <38392148+dongweixia521@users.noreply.github.com>
2018-05-16 12:44:57 +0800

    Update dwxia.js

--- dwxia.js
+++ dwxia.js
@@ -7,14 +7,14 @@ var links = gallery.getElementByTagName("a");
 for(var i=0;i<links.length;i++){
 links[i].onclick = function(){
 return showPic(this) ? false:true;}}}
-function showPic{
+function showPic(whichpic){
 if(!document.getElementById("dwx33")) return false;
 var source = whichpic.getElementById("href");
 var placeholder = document.getElementById ("dwx33");
 if (placeholder.nodeName !="IMG") return false;
 placeholder.setAttribute("src",source);
 if(!document.getElementById("description")){
-var text = whichpic.getAttribute("title")? whichpic.getAttribute ("title")? 
+var text = whichpic.getAttribute("title")? whichpic.getAttribute ("title"):"";
 var description = document.getElementById("description");
 if(description.firstChild.nodeType == 3){
 description.firstChild.nodeType = text;}}
